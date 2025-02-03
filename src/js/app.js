76ec9044a19a7b3021e01371e9faccfe92cda0dc
Damian Arrieta <damian.el.grinch@gmail.com>
2025-02-03 00:40:34 -0300

    Agregado de nuevas funciones, localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+
+
 document.querySelector('.header__form').addEventListener('submit', (event) =>{
     event.preventDefault();
     console.log("Se hizo click pero no reinicio la pagina");
@@ -8,28 +10,55 @@ document.querySelector('.header__form').addEventListener('submit', (event) =>{
     
     if(text === '') return;
 
-    const mainList = document.querySelector('.main__list');
+    const task = {
+        Texto: textarea.value.trim()
+    };
 
-    const tareaHTML = `
+    //Obtiene las tareas existentes de local Storage y las almacena en un array
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    //Agrega la nueva tarea al array
+    tasks.push(task);
+
+    //Almacena el array de tareas en local Storage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    //Muestra las tareas en pantalla
+    mostrarTareas();
+
+    textarea.value = "";
+    textarea.focus();
+});
+
+function mostrarTareas() {
+    const tareasGuardadas = document.querySelector('.main__list');
+    tareasGuardadas.innerHTML = "";
+
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    tasks.forEach(task => {
+        const taskHTML = `
         <div class="list__tarea">
                     <div class="tarea__checkbox">
                         <i class="fa-regular fa-square checkbox"></i>
                     </div>
-                    <p class="tarea__text">${text}</p>
+                    <p class="tarea__text">${task.Texto}</p>
                     <div class="tarea__botones">
                         <div class="tarea__boton editar"><i class="fa-regular fa-pen-to-square"></i></div>
                         <div class="tarea__boton eliminar"><i class="fa-regular fa-trash-can"></i></div>
                     </div>
         </div>`;
 
-    mainList.insertAdjacentHTML('beforeend', tareaHTML);
-
-    textarea.value = "";
-    textarea.focus();
-});
+        tareasGuardadas.insertAdjacentHTML('beforeend', taskHTML);
+    });
+}
 
 document.querySelector('.main__list').addEventListener('click', (event) => {
     if(event.target.closest('.eliminar')){
+        const tarea = event.target.closest('.list__tarea');
+        const textTarea = tarea.querySelector('.tarea__text').textContent;
+        
+        eliminarTarea(textTarea);
         event.target.closest('.list__tarea').remove();
     }
 
@@ -45,4 +74,20 @@ document.querySelector('.main__list').addEventListener('click', (event) => {
 
 });
 
+function obtenerTareas(){
+    return JSON.parse(localStorage.getItem('tasks')) || [];
+}
+
+function guardarTareas(tareas){
+    localStorage.setItem('tasks', JSON.stringify(tareas))
+}
+
+function eliminarTarea(textTarea){
+    let tareas = obtenerTareas();
+    tareas = tareas.filter(task => task.Texto!== textTarea);
+    guardarTareas(tareas);
+    mostrarTareas();
+}
+
+window.onload = mostrarTareas();
 
